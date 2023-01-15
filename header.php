e0112dad3b1b541a253cac088b371bc048e8e0f9
Manoj kumar <39759933+ManojMaheshwari@users.noreply.github.com>
2023-01-15 13:13:14 +0100

    Update header.php

--- header.php
+++ header.php
@@ -14,9 +14,18 @@
 <!doctype html>
 <html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
 <head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>	
 	<meta charset="<?php bloginfo("charset"); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<?php wp_head(); ?>
+<script>
+	jQuery(document).ready(function(){
+		jQuery(".mnav-title").click(function(){
+			jQuery(".mnav-inn").slideToggle("slow");
+		});
+	});	
+		
+	</script>	
 </head>
 
 <body <?php body_class(); ?>>
@@ -33,11 +42,17 @@
 			<div class="logo"> <!-- logo start here -->
 			<a href="<?php echo esc_url(home_url("/")); ?>"> <?php the_custom_logo(); ?></a>
 			</div>  <!-- logo end here -->
-			
+				<div class="mobile-nav">
+<span class="mnav-title">Menu</span>
+<div class="mnav-inn">
 			<div class="nav"> <!-- nav start here -->
 			<?php wp_nav_menu(["theme_location" => "primary"]); ?>
 				
 			</div> <!-- nav end here -->
+					</div>			
+		</div>	
+
+</div>	
 			
 			
 		</div>  <!-- header end here -->
